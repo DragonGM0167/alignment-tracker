08f99ec178f61266605673cd2ac1cd5861a97aea
DragonGM <dragongm0167@gmail.com>
2024-05-15 22:09:55 -0500

    Removed the trackerId methods

--- scripts/alignment-tracker.js
+++ scripts/alignment-tracker.js
@@ -31,21 +31,6 @@ class AlignmentTracker {
         return game.users.get(userId)?.getFlag(AlignmentTracker.ID, AlignmentTracker.FLAGS.ALIGNMENT);
     }
 
-    // Get the tracker by tracker id
-    static getByTrackerId(trackerId) {
-        const allTrackers = this.getAllTrackers();
-        let keys = allTrackers.keys();
-        while (true) {
-            let result = keys.next();
-        }
-        for (let [key, value] of allTrackers) {
-            if (value.trackerId == trackerId) {
-                return value;
-            }
-        }
-        return null;
-    }
-
     // Create an alignment tracker for a user
     static create(userId, actorId) {
         // Generate a new alignment tracker for user id
@@ -65,7 +50,8 @@ class AlignmentTracker {
     }
 
     // Update an alignment tracker with values
-    static update(tracker, trackerData) {
+    static update(actorId, trackerData) {
+        const tracker = this.update(this.getAllTrackers()[actorId], trackerData);
         // If the caller supplies invalid elements, ignore the request.
         if (trackerData.chaosLevel < this.LAWFUL || trackerData.chaosLevel > this.CHAOTIC ||
             trackerData.evilLevel < this.GOOD || trackerData.evilLevel > this.EVIL) {
@@ -84,18 +70,9 @@ class AlignmentTracker {
         return game.users.get(tracker.userId)?.setFlag(AlignmentTracker.ID, AlignmentTracker.FLAGS.ALIGNMENT, trackers);
     }
 
-    static updateByActorId(actorId, trackerData) {
-        return this.update(this.getAllTrackers()[actorId], trackerData);
-    }
-
-    static updateByTrackerId(trackerId, trackerData) {
-        const tracker = this.getByTrackerId(trackerId);
-        console.log(tracker);
-        return this.update(tracker, trackerData);
-    }
-
     // Adjust the chaotic scale for tracker.
-    static adjustChaotic(tracker, adjustment) {
+    static adjustChaotic(actorId, adjustment) {
+        const tracker = this.update(this.getAllTrackers()[actorId], trackerData);
         const chaosLevel = tracker.chaosLevel + adjustment;
         if (chaosLevel < this.LAWFUL) {
             return this.update(tracker, { chaosLevel: this.LAWFUL });
@@ -106,19 +83,9 @@ class AlignmentTracker {
         return this.update(tracker, { chaosLevel: chaosLevel });
     }
 
-    static adjustChaoticByActorId(actorId, adjustment) {
-        return this.adjustChaotic(this.getAllTrackers()[actorId], adjustment);
-    }
-
-    static adjustChaoticByTrackerId(trackerId, adjustment) {
-        const tracker = this.getByTrackerId(trackerId);
-        console.log(tracker);
-
-        return this.adjustChaotic(tracker, adjustment);
-    }
-
     // Adjust the evil scale for tracker.
-    static adjustEvil(tracker, adjustment) {
+    static adjustEvil(actorId, adjustment) {
+        const tracker = this.update(this.getAllTrackers()[actorId], trackerData);
         const evilLevel = tracker.chaosLevel + adjustment;
         if (evilLevel < this.GOOD) {
             return this.update(tracker, { evilLevel: this.GOOD });
@@ -129,48 +96,33 @@ class AlignmentTracker {
         return this.update(tracker, { evilLevel: evilLevel });
     }
 
-    static adjustEvilByActorId(actorId, adjustment) {
-        return this.adjustEvil(this.getAllTrackers()[actorId], adjustment);
-    }
-
-    static adjustEvilByTrackerId(trackerId, adjustment) {
-        return this.adjustEvil(this.getByTrackerId(trackerId), adjustment);
-    }
-    
     // Delete an alignment tracker
-    static delete(tracker) {
+    static delete(actorId) {
+        const tracker = this.update(this.getAllTrackers()[actorId], trackerData);
         const trackers = {
             [`-=${tracker.actorId}`]: null
         }
         return game.users.get(tracker.userId)?.setFlag(AlignmentTracker.ID, AlignmentTracker.FLAGS.ALIGNMENT, trackers);
     }
     
-    static deleteByActorId(actorId) {
-        return this.delete(this.getAllTrackers()[actorId]);
-    }
-
-    static deleteByTrackerId(trackerId) {
-        return this.delete(this.getByTrackerId(trackerId));
-    }
-
     static deleteByUserId(userId) {
         const allTrackers = this.getAllTrackers();
-        for (let ptr = 0; ptr < allTrackers.length; ptr++) {
-            const tracker = allTrackers[ptr];
-            if (tracker.userId == userId) {
-                return this.delete(tracker);
+        game.users.reduce((accumulator, user) => {
+            if (user.userId == userId) {
+                return {
+                    ...game.users.get(userId)?.setFlag(AlignmentTracker.ID, AlignmentTracker.FLAGS.ALIGNMENT, {})
+                }
             }
-        }
+        }, {});
     }
     
     // Delete all alignment trackers
     static deleteAll() {
-        const results = game.users.reduce((accumulator, user) => {
+        game.users.reduce((accumulator, user) => {
             return {
                 ...this.deleteByUserId(user.userId)
             }
         }, {});
-        return results;
     }
 
     static log(force, ...args) {  
